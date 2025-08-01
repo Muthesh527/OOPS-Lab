9de34d5cfceda942faa97306cf4ca6613ea4c60d
Muthesh527 <muthesh.240196@aids.ritchennai.edu.in>
2025-08-01 14:57:54 +0530

    Add files via upload

---
+++ Exp1.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int sum=0;
+		int n=100;
+		
+		for(int i=1;i<=n;i++) {
+			sum+=i;
+		}
+		System.out.println(sum);
+	}
+
+}
