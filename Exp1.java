6772bd48fdb2950a5ab5b3150e03ffac5cf8a7f6
Muthesh527 <muthesh.240196@aids.ritchennai.edu.in>
2025-08-03 16:27:21 +0530

    Add files via upload

--- Exp1.java
+++ Exp1.java
@@ -1,12 +1,10 @@
-package Test;
-
-public class Test {
+public class  Exp1{
 
 	public static void main(String[] args) {
-		int sum=0;
-		int n=100;
+		int sum=0,end=100,start=0;
+//		int n=100;
 		
-		for(int i=1;i<=n;i++) {
+		for(int i=start;i<=end;i++) {
 			sum+=i;
 		}
 		System.out.println(sum);
